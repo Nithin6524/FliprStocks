91d7fee0b766c9f4be1c2565d66395e89749ecdd
MVC <charan29062005@gmail.com>
2025-04-12 16:25:53 +0530

    pre_final

--- Backend/src/controllers/earningsController.js
+++ Backend/src/controllers/earningsController.js
@@ -12,8 +12,8 @@ const handleResponse=(res,status,message,data=null)=>{
 export const getEarnings=async(req,res,next)=>{
     try{
         const stock=await getEarningsService (req.params.id);
-        if(!stock)return handleResponse(res,404,"Overview for stock not found");
-        handleResponse(res,200,"Overview for stock fetched successfully",stock);
+        if(!stock)return handleResponse(res,404,"Earnings for stock not found");
+        handleResponse(res,200,"Earnings for stock fetched successfully",stock);
     }catch(error){
         next(error);
     }
--- Backend/src/models/earnings_Model.js
+++ Backend/src/models/earnings_Model.js
@@ -2,6 +2,9 @@ import pool from "../config/db.js";
 
 
 export const getEarningsService = async (id) => {
-    const result = await pool.query('select * from income_stmt,balance_sheet where ticker=$1',[id]);
+    const result = await pool.query(`
+    SELECT distinct * from 
+    income_stmt 
+    WHERE ticker = $1`,[id]);
     return result.rows;
 };
