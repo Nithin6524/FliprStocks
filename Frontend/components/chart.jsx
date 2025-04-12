6f94d4e68269d8316cd702804e957204cc79d6a3
M SAI NITHIN <nithin6524@gmail.com>
2025-04-12 17:10:34 +0530

    Revert "charts are done"
    This reverts commit 53c38967f959691e0b4e9b75ec3140277b4c2f54.

--- Frontend/components/chart.jsx
+++ Frontend/components/chart.jsx
@@ -133,7 +133,7 @@ const LightweightChartComponent = () => {
             chartContainerRef.current,
             {
                 layout: {
-                    background: { color: "#020b1f"},
+                    background: { color: "#222" },
                     textColor: "#C3BCDB",
                 },
                 grid: {
@@ -257,7 +257,7 @@ const LightweightChartComponent = () => {
             {/* Chart container */}
             <div
                 ref={chartContainerRef}
-                style={{ position: "absolute", width: "100%", height: "45%" }}
+                style={{ position: "absolute", width: "100%", height: "100%" }}
             />
         </div>
     );
